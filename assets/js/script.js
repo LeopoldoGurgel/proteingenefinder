2d10ff0d3560426fc8f47fa97717226ff15bf1d0
rileysong01 <rsong47@uwo.ca>
2023-08-27 01:35:29 -0400

    changed all BLAST variables to domain

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@
 var NCBIAPIKey = 'd019ce82781c44b8ac9d2547bbc391e9a908';
 
 $("#submitBtn").on("click", function (event) {
-    $('#table-of-contents').removeClass('initalHide')
+    $('#table-of-contents').removeClass('initialHide')
+    $('#mainSection').removeClass('initialHide')
     event.preventDefault();
 
     var userGene = $("#geneInput").val();
