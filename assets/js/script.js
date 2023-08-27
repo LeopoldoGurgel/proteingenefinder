c9ad1ceb805c46571e01c75d57e04eeaaf74ac68
LeopoldoGurgel <120462695+LeopoldoGurgel@users.noreply.github.com>
2023-08-27 00:43:12 -0400

    history saves to local storage and shows in a dropdown under the input

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,11 @@ $("#submitBtn").on("click", function(event){
 
     var userGene = $("#geneInput").val();
     var userSpecies = 'homo sapiens'
+    
+    // saves the search input to local history
+    if(userGene !== " "){
+        updateSearchHistory(userGene);
+    }
 
     fetchAccessionID(userGene, userSpecies)
 });
@@ -264,19 +269,6 @@ $("#aaBtn").on("click", function(){
     $(this).removeClass("is-light").addClass("is-info");
 })
 
-$("#blastBtn").on("click", function(){
-    var textToCopy = $("#blastText").text();
-    var clipboard = $("<textarea>"); //will not appear, will just temporarely hold the value.
-    $("body").append(clipboard);
-    // selects the content of the text area containing the aaText
-    clipboard.val(textToCopy).select();
-    // copies the text
-    document.execCommand("copy");
-    clipboard.remove();
-    $(this).text("Copied!");
-    $(this).removeClass("is-light").addClass("is-info");
-})
-
 
 // was trying to fix a bug by clearing everything as the first
 // function called when the search button was clicked.
@@ -329,5 +321,76 @@ function getGenbankInfo(ID, key) {
         });
 }
 
+
+// saves geneInput entry to local storage
+var geneDropdown = $("#geneDropdown");
+var geneInput = $("#geneInput");
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory"))||[];
+
+function updateSearchHistory(value){
+    
+    // this makes the local storage only keep 5 indexes.
+    // since push make the new index go to the end of the array
+    // the shift method takes out the first index.
+    if(searchHistory.length >= 5){
+        searchHistory.shift();
+    }
+
+    searchHistory.push(value);
+
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+}
+
+// displays searchHistory in the dropdown menu
+
+function displayHistory() {
+    geneDropdown.empty();
+    
+    searchHistory.forEach(value => {
+        var entry = $("<a>").text(value);
+        entry.on("click", function(){
+            geneInput.val(value);
+            // hide and show methods add the display: none rule and 
+            // take back again. This would have saved me some time 
+            // a few lines ago.
+            geneDropdown.addClass("hidden");
+        });
+        geneDropdown.append(entry);
+    })
+}
+
+geneInput.on("mousedown", function(event){
+    event.preventDefault();
+    displayHistory();
+    geneDropdown.removeClass("hidden");
+})
+
+geneInput.on("blur", function(){
+    geneDropdown.addClass("hidden");
+})
+
+$(document).on("click", function(event) {
+    if (
+      !$(event.target).is(geneInput) &&
+      !$(event.target).closest(geneDropdown).length
+    ) {
+      geneDropdown.addClass("hidden");
+    }
+  });
+
+geneInput.on("keyup", function(event){
+    if(event.key === "Enter"){
+        var value = geneInput.val().trim();
+        if(value !== " "){
+            updateSearchHistory(value);
+        }
+    }
+})
+
+geneDropdown.on("click", "a", function() {
+    var value = $(this).text();
+    geneInput.val(value);
+    geneDropdown.addClass("hidden");
+  });
 
 // fetch(`https://rest.uniprot.org/uniprotkb/search?query=CFTR+AND+organism_name:human+AND+reviewed:true&fields=accession,xref_pdb,xref_ensembl&format=json&size=2`)
